6dd0ff2c746ad2421fbbb969decf2f6ce8b497ad
dummmylitt <109826760+dummmylitt@users.noreply.github.com>
2024-04-15 14:18:11 -0700

    Add files via upload

--- button.py
+++ button.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 from signal import pause
-
+import json
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
@@ -16,11 +16,19 @@ def setTimer(t):
         t-=1
     print('Good job brushing!')
 t = 120
-    
+
+def displayData():
+    # read from data.json
+    tooth_j_file = open('data.json', "r")
+    data = json.load(tooth_j_file.read())
+    for i in data['emp_details']:
+        print(i)
+    f.close()
 try:
     while True:
         if (GPIO.input(23)):
             setTimer(t)
+            displayData()
 except KeyboardInterrupt:
     pass
     
