a67c3df9cfe1569bbcc3d3d3cf4d0e00ec6335df
dummmylitt <109826760+dummmylitt@users.noreply.github.com>
2024-04-15 23:06:44 -0700

    Add files via upload

--- button.py
+++ button.py
@@ -1,35 +1,52 @@
 import RPi.GPIO as GPIO
 import time
 from signal import pause
-import os
+import json
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-#tap1 = Button(2) #Switch that detects tap handle position
 
-def setTimer(t):
-    while t:
-        mins, secs = divmod(t, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
-        time.sleep(1)
-        t-=1
+def setTimer(duration):
+    stopwatch = 0
+    while True:
+        if duration >= 0:
+            mins_timer, secs_timer = divmod(duration, 60)
+            timer_display = '{:02d}:{:02d}'.format(mins_timer, secs_timer)
+
+            mins_stopwatch, secs_stopwatch = divmod(stopwatch, 60)
+            stopwatch_display = '{:02d}:{:02d}'.format(mins_stopwatch, secs_stopwatch)
+
+            print("TIMER: ", timer_display, end="\r")
+            print("STOPWATCH: ", stopwatch_display, end="\r")
+            time.sleep(1)
+            
+            duration -= 1
+            stopwatch += 1
+        if (GPIO.input(23)):
+            return stopwatch_display
+            print("returned stopwatch_display!")
+            break
+
     print('Good job brushing!')
-t = 120
 
 def displayData():
-    # Change this to the ABSOLUTE path of toothbrush.py on the rpi
-    os.system("python toothbrush.py --show")
-
-
+    # read from data.json
+    tooth_j_file = open('toothbrush.json', "r")
+    data = json.loads(tooth_j_file.read())
+    for i in data['emp_details']:
+        print(i)
+    tooth_j_file.close()
+    
 try:
     while True:
         if (GPIO.input(23)):
-            setTimer(t)
-            displayData()
+            stopwatch_time = setTimer(120)
+                
+            #if GPIO 23 is pressed again
+                #write to json file: toothbrush.json the stopwatch time at the point of the second button press
+        #if (GPIO.input(25)):
+            #displayData()
 except KeyboardInterrupt:
     pass
     
-
 GPIO.cleanup()
-
