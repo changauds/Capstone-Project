5ed73b05c274edcd67360996d85fd7fee90a8d1a
Alex Ghandhi <alexpghandhi@gmail.com>
2024-04-16 16:59:11 -0700

    change this

--- button.py
+++ button.py
@@ -1,52 +1,15 @@
-import RPi.GPIO as GPIO
-import time
-from signal import pause
-import json
+match activeBrushing:
+    case True:
+        # Stop the timer, get timer value
+        # Convert value to minutes (as a float)
+        # call toothbrush --store [time]
+        # set activeBrushing to false
+        # delay
+        # run toothbrush --show
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+    case False:
+        # switch activeBrushing to True
+        # start timer
 
-def setTimer(duration):
-    stopwatch = 0
-    while True:
-        if duration >= 0:
-            mins_timer, secs_timer = divmod(duration, 60)
-            timer_display = '{:02d}:{:02d}'.format(mins_timer, secs_timer)
-
-            mins_stopwatch, secs_stopwatch = divmod(stopwatch, 60)
-            stopwatch_display = '{:02d}:{:02d}'.format(mins_stopwatch, secs_stopwatch)
-
-            print("TIMER: ", timer_display, end="\r")
-            print("STOPWATCH: ", stopwatch_display, end="\r")
-            time.sleep(1)
-            
-            duration -= 1
-            stopwatch += 1
-        if (GPIO.input(23)):
-            return stopwatch_display
-            print("returned stopwatch_display!")
-            break
-
-    print('Good job brushing!')
-
-def displayData():
-    # read from data.json
-    tooth_j_file = open('toothbrush.json', "r")
-    data = json.loads(tooth_j_file.read())
-    for i in data['emp_details']:
-        print(i)
-    tooth_j_file.close()
-    
-try:
-    while True:
-        if (GPIO.input(23)):
-            stopwatch_time = setTimer(120)
-                
-            #if GPIO 23 is pressed again
-                #write to json file: toothbrush.json the stopwatch time at the point of the second button press
-        #if (GPIO.input(25)):
-            #displayData()
-except KeyboardInterrupt:
-    pass
-    
-GPIO.cleanup()
+    case other:
+        print("Error")
