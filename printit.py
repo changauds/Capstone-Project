a2ebe41ee06daf734deb9a0358c6bd496d73fdb3
dummmylitt <109826760+dummmylitt@users.noreply.github.com>
2024-05-01 13:46:30 -0700

    Simple print function for the touch button module

---
+++ printit.py
@@ -0,0 +1,10 @@
+import time
+import os
+
+
+def hello():
+    while(not time.sleep(10)):
+        os.system("python /home/group6/MagicMirror/modules/MMM-PythonPrint/toothbrush.py --show")
+
+if __name__ == '__main__':
+    hello()
